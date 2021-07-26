3186160412a12c4e1ad70addf512c5785896bffe
AndGut49 <andie.gtz00@gmail.com>
2021-07-26 00:42:07 -0500

    first commit

---
+++ app/src/main/java/com/libros/tinderlibros/Signin.kt
@@ -0,0 +1,11 @@
+package com.libros.tinderlibros
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Signin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
