ed3f95b432d085d8cade150d97ee8fcb4f2d1874
AndGut49 <andie.gtz00@gmail.com>
2021-07-26 19:12:29 -0500

    Cambiando de Login a SignIn y de SignIn a LogIn

--- app/src/main/java/com/libros/tinderlibros/MainActivity.kt
+++ app/src/main/java/com/libros/tinderlibros/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.libros.tinderlibros
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var loginButton : Button
+    private lateinit var signinButton : Button
+    private lateinit var usernameInput : EditText
+    private lateinit var passwordInput : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signinButton =findViewById(R.id.signinButton)
+        signinButton.setOnClickListener{
+            var siginIntent = Intent(this,Signin::class.java)
+            startActivity(siginIntent)
+        }
+
     }
 }
