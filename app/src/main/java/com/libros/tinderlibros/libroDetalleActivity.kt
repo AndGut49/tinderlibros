42912199acbd2cd9a2bf0e735f918ffe5ccf3ba2
Jim Loza <jimloza25@gmail.com>
2021-07-29 10:30:02 -0500

    Terminando vista de detalles de libro

---
+++ app/src/main/java/com/libros/tinderlibros/libroDetalleActivity.kt
@@ -0,0 +1,11 @@
+package com.libros.tinderlibros
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class libroDetalleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_libro_detalle)
+    }
+}
