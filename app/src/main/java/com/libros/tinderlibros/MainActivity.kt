f1ab5aa4cbe590b28d5b3a8360a84031f933035f
AndGut49 <andie.gtz00@gmail.com>
2021-08-12 21:13:17 -0500

    fixing merges

--- app/src/main/java/com/libros/tinderlibros/MainActivity.kt
+++ app/src/main/java/com/libros/tinderlibros/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.libros.tinderlibros
 
 import android.content.Intent
-<<<<<<< HEAD
-
-=======
 import androidx.appcompat.app.AppCompatActivity
->>>>>>> jim
+
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputLayout
 
 val userC="developer"
